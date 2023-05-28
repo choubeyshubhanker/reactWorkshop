9c81bca968b5ccaf521549a85bcfccac2525f2cc
Shubhanker Choubey <shubhankerc@gmail.com>
2023-05-28 18:42:31 +0530

    improvised

--- src/components/MainLayout.jsx
+++ src/components/MainLayout.jsx
@@ -4,7 +4,7 @@ import Thumbnail from './listingLayout/thumbnail'
 
 const MainLayout = () => {
   return (
-    <main className='relative w-full overflow-x-hidden-hidden h-screen w-full'>
+    <main className='relative w-full overflow-x-hidden-hidden h-screen'>
     <Header/>
     <section className='listing h-full w-full absolute top-48 grid grid-cols-3 gap-y-24 gap-x-7.5 mx-auto px-7.5'>
     <Thumbnail/>
