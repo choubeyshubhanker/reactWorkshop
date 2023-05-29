199cb97789ab9e2e073e672735dd5922b9e02050
Shubhanker Choubey <shubhankerc@gmail.com>
2023-05-29 23:47:51 +0530

    long title slice

--- src/components/listingLayout/Thumbnail.jsx
+++ src/components/listingLayout/Thumbnail.jsx
@@ -8,13 +8,13 @@ const Thumbnail = ({name,image}) => {
   const onImageError = (e) => { // If image URL doesn't exist
     e.target.src = placeHolderImg
   }
-
+ const movieName = name.length<=15?name: name.slice(0,15)+"..."
 
   return (
     <figure className='text-left h-fit lg:m-auto' >
       <div className='test overflow-hidden'>
         <img className='cover mb-6' src={URL} alt="poster" loading="lazy" title="posterImg"  onError={onImageError} /></div>
-      <figcaption className='text-white text-12' > {name} </figcaption>
+      <figcaption className='text-white text-12' > {movieName} </figcaption>
     </figure>
   )
 }
