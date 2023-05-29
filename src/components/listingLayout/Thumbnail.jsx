74250bd01c44d97c3cce0956a23d498d03b699d4
Shubhanker Choubey <shubhankerc@gmail.com>
2023-05-29 12:35:00 +0530

    placeholder image

--- src/components/listingLayout/Thumbnail.jsx
+++ src/components/listingLayout/Thumbnail.jsx
@@ -5,12 +5,15 @@ const Thumbnail = ({name,image}) => {
   // console.log("name", image)
   const URL = `./Slices/${image}`? `./Slices/${image}` : placeHolderImg;
 
-  
+  const onImageError = (e) => {
+    e.target.src = placeHolderImg
+  }
+
 
   return (
     <figure className='text-left h-fit lg:m-auto' >
-      <div className='test'>
-        <img className='cover mb-6' src={URL} alt="poster" loading="lazy" /></div>
+      <div className='test overflow-hidden'>
+        <img className='cover mb-6' src={URL} alt="poster" loading="lazy"  onError={onImageError} /></div>
       <figcaption className='text-white text-12' > {name} </figcaption>
     </figure>
   )
